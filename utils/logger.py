a4c04d8f122453c0bd40f9aec855a2713e97856c
dovehub <sorrydongdongdong@gmail.com>
2020-09-25 16:57:11 +0800

    优化

--- utils/logger.py
+++ utils/logger.py
@@ -2,6 +2,7 @@ import os
 import sys
 import copy
 from functools import wraps
+from typing import Optional, Union
 
 from loguru import logger as _logger
 
@@ -12,24 +13,33 @@ __all__ = [
 _logger.remove()
 
 
-def Log(prefix: str) -> _logger:
-    basedir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+def Log(prefix: str,
+        path: Optional[str] = None,
+        format: Optional[str] = None,
+        level: Union[str, int] = 'WARNING',
+        rotation: str = "00:00",
+        retention: str = "30 days",
+        enqueue: bool = False) -> _logger:
+    if path is None:
+        basedir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-    # 定位到log日志文件
-    log_path = os.path.join(basedir, 'logs')
+        # 定位到log日志文件
+        path = os.path.join(basedir, 'logs')
 
-    if not os.path.exists(log_path):
-        os.makedirs(log_path)
+    if not os.path.exists(path):
+        os.makedirs(path)
 
-    file_path = os.path.join(log_path, f'{prefix}.log')
-    format = f"<green>{{time:YYYY-MM-DD HH:mm:ss.SSS}}</green> | <level>{{level: <8}}</level> | <cyan>{{name}}</cyan>:<cyan>{{function}}</cyan>:<cyan>{{line}}</cyan> | <fg #FFC0CB>{prefix}</fg #FFC0CB> - <level>{{message}}</level>"
+    file_path = os.path.join(path, f'{prefix}.log')
+    if format is None:
+        format = f"<green>{{time:YYYY-MM-DD HH:mm:ss.SSS}}</green> | <level>{{level: <8}}</level> | <cyan>{{name}}</cyan>:<cyan>{{function}}</cyan>:<cyan>{{line}}</cyan> | <fg #FFC0CB>{prefix}</fg #FFC0CB> - <level>{{message}}</level>"
     logger = copy.deepcopy(_logger)
-    logger.add(sys.stdout, format=format)
+    logger.add(sys.stdout, format=format, enqueue=enqueue)
     logger.add(file_path,
                format=format,
-               level="WARNING",
-               rotation="00:00",
-               retention="30 days")
+               level=level,
+               rotation=rotation,
+               retention=retention,
+               enqueue=enqueue)
     return logger
 
 
